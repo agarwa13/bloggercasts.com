7b8281065ec67bc40856780775fa8019b528104d
Nikhil Agarwal <nikhilagarwal0963@gmail.com>
2017-02-04 17:47:52 -0800

    added cron job to clean up notifications each night

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,8 +3,10 @@
 namespace App\Console;
 
 use App\Mail\WeeklySummaryForAdmin;
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 
 class Kernel extends ConsoleKernel
@@ -38,6 +40,23 @@ class Kernel extends ConsoleKernel
         // Populate the Metrics in the Spark Dashboard
         $schedule->command('spark:kpi')->dailyAt('23:55');
 
+        // Remove Notifications and Announcements
+        $schedule->call(function(){
+
+            // Delete all notifications that are at least two weeks old
+            DB::table('notifications')
+                ->where('created_at','<',Carbon::now()->subWeek(2)->toDateTimeString())->delete();
+
+            // Delete Notifications that are Read and at least a Week old
+            DB::table('notifications')
+                ->where('read',true)
+                ->where('created_at','<',Carbon::now()->subWeek()->toDateTimeString())->delete();
+
+            // Delete Announcements that are one or more old
+            DB::table('announcements')
+                ->where('created_at','<',Carbon::now()->subYear()->toDateTimeString())->delete();
+
+        })->dailyAt('23:56');
     }
 
     /**
