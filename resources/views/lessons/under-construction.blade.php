6f46f84b8e458cdf84d22a0a9207ddbedb23044c
Nikhil Agarwal <nikhilagarwal0963@gmail.com>
2017-02-07 17:01:48 -0800

    updated under construction view to remove mailerlite subscribe form

--- resources/views/lessons/under-construction.blade.php
+++ resources/views/lessons/under-construction.blade.php
@@ -13,44 +13,44 @@
             <span class="Heading--Fancy__subtitle" style="padding-bottom: 10px;">
             This Lesson is Not Published Yet
         </span>
-            <span>Subscribe To Be Notified When It Is</span>
+            <span>Check back in a couple of days</span>
         </h2>
 
-        <div class="col-md-4 col-md-offset-4">
+        {{--<div class="col-md-4 col-md-offset-4">--}}
 
-            <div class="panel panel-default">
-                <div class="panel-body">
+            {{--<div class="panel panel-default">--}}
+                {{--<div class="panel-body">--}}
 
-                    @if (count($errors) > 0)
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
+                    {{--@if (count($errors) > 0)--}}
+                        {{--<div class="alert alert-danger">--}}
+                            {{--<ul>--}}
+                                {{--@foreach ($errors->all() as $error)--}}
+                                    {{--<li>{{ $error }}</li>--}}
+                                {{--@endforeach--}}
+                            {{--</ul>--}}
+                        {{--</div>--}}
+                    {{--@endif--}}
 
-                    <form method="post" action="{{url('/subscribe')}}" target="_blank">
-                        {!! csrf_field() !!}
+                    {{--<form method="post" action="{{url('/subscribe')}}" target="_blank">--}}
+                        {{--{!! csrf_field() !!}--}}
 
-                        <input type="hidden" name="lesson_id" value="{{$lesson->id}}">
+                        {{--<input type="hidden" name="lesson_id" value="{{$lesson->id}}">--}}
 
-                        <div class="form-group">
-                            <label for="email">Email address</label>
-                            <input type="email" class="form-control" id="email" name="email" placeholder="Email">
-                        </div>
+                        {{--<div class="form-group">--}}
+                            {{--<label for="email">Email address</label>--}}
+                            {{--<input type="email" class="form-control" id="email" name="email" placeholder="Email">--}}
+                        {{--</div>--}}
 
-                        <div class="form-group">
-                            <label for="name">Name</label>
-                            <input type="text" class="form-control" id="name" name="name" placeholder="Name">
-                        </div>
+                        {{--<div class="form-group">--}}
+                            {{--<label for="name">Name</label>--}}
+                            {{--<input type="text" class="form-control" id="name" name="name" placeholder="Name">--}}
+                        {{--</div>--}}
 
-                        <button type="submit" class="btn btn-primary">Subscribe</button>
-                    </form>
-                </div>
-            </div>
+                        {{--<button type="submit" class="btn btn-primary">Subscribe</button>--}}
+                    {{--</form>--}}
+                {{--</div>--}}
+            {{--</div>--}}
 
-        </div>
+        {{--</div>--}}
     </div>
 </div>
