9d9d1767d3bc5b7aeecb375b1f3e83f7e1dfc451
Nikhil Agarwal <nikhilagarwal0963@gmail.com>
2017-02-12 12:06:40 -0800

    updated nav to include services for both guest and logged in users

--- resources/views/vendor/spark/nav/guest.blade.php
+++ resources/views/vendor/spark/nav/guest.blade.php
@@ -23,6 +23,30 @@
 
             <!-- Right Side Of Navbar -->
             <ul class="nav navbar-nav navbar-right">
+
+                <!-- Right Side Of Navbar -->
+                <li class="dropdown">
+                    <!-- User Photo / Name -->
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                        Services
+                        {{--<img :src="user.photo_url" class="spark-nav-profile-photo m-r-xs">--}}
+                        {{--<span class="caret"></span>--}}
+                    </a>
+
+                    <!-- Google Analytics Related Services-->
+                    <ul class="dropdown-menu" role="menu">
+
+                        <li class="dropdown-header">Google Analytics</li>
+
+                        <!-- Eliminate Spam -->
+                        <li>
+                            <a href="/services/eliminate-analytics-spam">
+                                <i class="fa fa-fw fa-btn fa-user-secret"></i>Eliminate Spam
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
                 <li><a href="/login" class="navbar-link">Login</a></li>
                 <li><a href="/register" class="navbar-link">Register</a></li>
             </ul>
--- resources/views/vendor/spark/nav/user-right.blade.php
+++ resources/views/vendor/spark/nav/user-right.blade.php
@@ -1,1 +1,22 @@
 <!-- Right Side Of Navbar -->
+<li class="dropdown">
+    <!-- User Photo / Name -->
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+        Services
+        {{--<img :src="user.photo_url" class="spark-nav-profile-photo m-r-xs">--}}
+        {{--<span class="caret"></span>--}}
+    </a>
+
+    <!-- Google Analytics Related Services-->
+    <ul class="dropdown-menu" role="menu">
+
+        <li class="dropdown-header">Google Analytics</li>
+
+        <!-- Eliminate Spam -->
+        <li>
+            <a href="/services/eliminate-analytics-spam">
+                <i class="fa fa-fw fa-btn fa-user-secret"></i>Eliminate Spam
+            </a>
+        </li>
+    </ul>
+</li>
