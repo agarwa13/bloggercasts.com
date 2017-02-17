77ffc8b4cdf773f494650c8f074be67b223e7110
Nikhil Agarwal <nikhilagarwal0963@gmail.com>
2017-02-16 21:07:09 -0800

    added facebook pages verification

--- resources/views/layouts/includes/site-verifications.blade.php
+++ resources/views/layouts/includes/site-verifications.blade.php
@@ -1,3 +1,4 @@
 <!-- TradeDoubler site verification 2886177 -->
 <!-- Pinterest Site Verification -->
 <meta name="p:domain_verify" content="d3396959e70afabd8d5070e6da2e32a3"/>
+<meta property="fb:pages" content="299045260464685" />
