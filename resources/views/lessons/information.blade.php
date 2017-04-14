f3f43e437d148589e39c52f567a7a0524b42797f
Nikhil Agarwal <nikhilagarwal0963@gmail.com>
2017-04-13 19:58:58 -0700

    removed Downloads Box with Discuss Lesson

--- resources/views/lessons/information.blade.php
+++ resources/views/lessons/information.blade.php
@@ -2,16 +2,16 @@
 
 
 
-    <div class="Video__buttons Box">
-        <ul class="utility-naked-list">
-            <li>
-                <a href="#disqus_thread" class="Button Button--with-icon">
-                    <i class="material-icons">comment</i>
-                    <span>Discuss Lesson</span>
-                </a>
-            </li>
-        </ul>
-    </div>
+    {{--<div class="Video__buttons Box">--}}
+        {{--<ul class="utility-naked-list">--}}
+            {{--<li>--}}
+                {{--<a href="#disqus_thread" class="Button Button--with-icon">--}}
+                    {{--<i class="material-icons">comment</i>--}}
+                    {{--<span>Discuss Lesson</span>--}}
+                {{--</a>--}}
+            {{--</li>--}}
+        {{--</ul>--}}
+    {{--</div>--}}
 
 
 
